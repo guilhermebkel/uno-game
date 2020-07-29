7b5adb931aff0d985dc597570873cceb8a859127
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-07-29 19:02:55 -0300

    refactor(*): improve layout for 6 players

--- packages/unoenty/src/pages/Table/index.tsx
+++ packages/unoenty/src/pages/Table/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Grid, Button, Typography } from "@material-ui/core"
+import { Grid, Button } from "@material-ui/core"
 import { useParams, useHistory } from "react-router-dom"
 import { DndProvider } from "react-dnd"
 import { HTML5Backend } from "react-dnd-html5-backend"
@@ -10,7 +10,7 @@ import { useSocketStore } from "@/store/Socket"
 import useDidMount from "@/hooks/useDidMount"
 import useSocket from "@/hooks/useSocket"
 
-import { LoadingComponent, Alert } from "@/components"
+import { LoadingComponent, Alert, Divider } from "@/components"
 
 import Device from "@/utils/device"
 
@@ -115,16 +115,6 @@ const Table = () => {
 									player={socket.otherPlayers?.[1] as any}
 									transform="rotate(135deg)"
 								/>
-								{socket?.currentPlayer?.canBuyCard && (
-									<Button
-										color="primary"
-										variant="contained"
-										onClick={buyCard}
-										fullWidth
-									>
-										BUY CARD
-									</Button>
-								)}
 							</Grid>
 							<Grid item xs={8}>
 								<Grid container justify="center" alignItems="center">
@@ -141,13 +131,6 @@ const Table = () => {
 									player={socket.otherPlayers?.[3] as any}
 									transform="rotate3d(2.5, 1, 0, 180deg)"
 								/>
-								<Typography
-									variant="h1"
-									align="center"
-									style={{ color: socketStore?.game?.currentGameColor }}
-								>
-									{!Device.isMobile && "Round"} {socketStore?.game?.round}
-								</Typography>
 							</Grid>
 						</Grid>
 						<Grid container alignItems="center">
@@ -166,6 +149,18 @@ const Table = () => {
 										cards={socketStore?.game?.usedCards as any}
 										onDrop={onDrop}
 									/>
+
+									<Divider size={5} />
+
+									{socket?.currentPlayer?.canBuyCard && (
+										<Button
+											color="primary"
+											variant="contained"
+											onClick={buyCard}
+										>
+											BUY CARD
+										</Button>
+									)}
 								</Grid>
 							</Grid>
 							<Grid item xs={2}>
