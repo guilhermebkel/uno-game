1868a64edd301c5b232055e3f21910034d6822bc
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-07-30 19:01:13 -0300

    chore(unapy): add caching for static files

--- packages/unapy/src/Controllers/AssetController.ts
+++ packages/unapy/src/Controllers/AssetController.ts
@@ -3,9 +3,13 @@ import express, { Request, Response, NextFunction } from "express"
 
 class AssetController {
 	getAsset (req: Request, res: Response, next: NextFunction) {
+		const thirtyDaysInMilliseconds = 2592000
+
 		const assetsPath = path.join(__dirname, "..", "Assets")
 
-		return express.static(assetsPath)(req, res, next)
+		return express.static(assetsPath, {
+			maxAge: thirtyDaysInMilliseconds
+		})(req, res, next)
 	}
 }
 
