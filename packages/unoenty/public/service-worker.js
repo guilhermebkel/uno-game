ebd134b4b71c829704649a5a2c85f2f1d6b1c06e
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2020-10-12 10:38:45 -0300

    chore(service-worker): remove extra comments

--- packages/unoenty/public/service-worker.js
+++ packages/unoenty/public/service-worker.js
@@ -1,13 +1,14 @@
-var CACHE_NAME = "my-web-app-cache"
+var CACHE_NAME = "uno-game-cache"
 var urlsToCache = ["/"]
 
 self.addEventListener("install", function (event) {
-	// event.waitUntil takes a promise to know how
-	// long the installation takes, and whether it
-	// succeeded or not.
+	/**
+	 * event.waitUntil takes a promise to know how
+	 * long the installation takes, and whether it
+	 * succeeded or not.
+	 */
 	event.waitUntil(
 		caches.open(CACHE_NAME).then(function (cache) {
-			console.log("Opened cache")
 			return cache.addAll(urlsToCache)
 		}),
 	)
